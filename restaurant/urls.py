b11b769406228531363608caecb41281c317e243
Nasir <nasirhossenmollah@gmail.com>
2026-09-12 20:37:32 +0600

    APIs created for Menu and Booking Model

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from .views import index, MenuItemView, SingleMenuItemView, BookingViewSet
+from rest_framework.routers import DefaultRouter
 
 
+router = DefaultRouter()
+router.register(r'booking/tables', BookingViewSet, basename='booking')
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
+    path('menu/items/', MenuItemView.as_view(), name='menu-items'),
+    path('menu/items/<int:pk>/', SingleMenuItemView.as_view(), name='single-menu-item'),
+    path('', include(router.urls)),
 ]
